97d550144c14a80d09b9376e3a71efa98a0e2738
Spm25 <manh25371@gmail.com>
2023-10-30 03:14:57 +0700

    add SanPham admin

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using shop.Models;
+using X.PagedList;
 
 namespace WebBanHang.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin")]
+    [Route("Admin/Home")]
     public class HomeController : Controller
     {
+        ShopContext db = new ShopContext();
+        [Route("")]
+        [Route("Index")]
         public IActionResult Index()
         {
             return View();
+        }
+        [Route("SanPham")]
+        public IActionResult SanPham(int? page) 
+        {
+            int pageSize = 3;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstSanPham = db.Products.AsNoTracking().OrderBy(x => x.Title);
+            PagedList<Product> lst = new PagedList<Product>(lstSanPham, pageNumber, pageSize);
+            return View(lst); 
         }
     }
 }
